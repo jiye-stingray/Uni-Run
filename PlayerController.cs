af041bb46b19f6e32d98b8c6827e060a8a48a002
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-05-27 11:51:48 +0900

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour {
    private bool isGrounded = false; // 바닥에 닿았는지 나타냄
    private bool isDead = false; // 사망 상태
 
-   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+    private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
    private Animator animator; // 사용할 애니메이터 컴포넌트
    private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
@@ -51,18 +51,48 @@ public class PlayerController : MonoBehaviour {
 
 
    private void Die() {
-       // 사망 처리
+        // 애니메이터 Die 트리거 파라미터를 셋
+        animator.SetTrigger("Die");
+
+        //오디오 소스에 할당된 오디오 클립을 deathCilp으로 변경
+        playerAudio.clip = deathClip;
+        //사망 효과음 재생
+        playerAudio.Play();
+
+        //속도를 제로(0,0)로 변경
+        playerRigidbody.velocity = Vector2.zero;
+        //사망 상태를 true로 변경
+        isDead = true;
+
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
-       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+        // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+
+        if (other.tag == "Dead" && !isDead)
+        {
+            //충돌한 상대방의 태그가 Dead이며 아직 사망하지 않았다면 Die() 실행
+            Die();
+        }
+        
    }
 
    private void OnCollisionEnter2D(Collision2D collision) {
-       // 바닥에 닿았음을 감지하는 처리
+        // 바닥에 닿았음을 감지하는 처리
+
+        //어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있으면
+        if (collision.contacts[0].normal.y>0.7f)
+        {
+            //isGround를 true로 변경하고, 누적 점프 횟수를 0으로 리셋
+            isGrounded = true;
+            jumpCount = 0;
+        }
    }
 
    private void OnCollisionExit2D(Collision2D collision) {
-       // 바닥에서 벗어났음을 감지하는 처리
+        // 바닥에서 벗어났음을 감지하는 처리
+
+        //어떤 콜라이더에서 떼어진 경우 isGrounded를 false로 변경
+        isGrounded = false;
    }
 }
