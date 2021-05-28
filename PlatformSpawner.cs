62b7b4ca6569b059dc59d754657ce9490ab013ea
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-05-28 13:42:28 +0900

    Add files via upload

--- PlatformSpawner.cs
+++ PlatformSpawner.cs
@@ -22,9 +22,37 @@ public class PlatformSpawner : MonoBehaviour {
 
     void Start() {
         // 변수들을 초기화하고 사용할 발판들을 미리 생성
+
+        //count만큼 루프 하면서 발판 생성
+        for (int i = 0; i < count; i++)
+        {
+            //platformfab을 원본으로 새 발판을 poolPosition
+            //생성된 발판을 platforms 배열에 할당
+            platforms[i] = Instantiate(platformPrefab, poolPosition, Quaternion.identity); ;
+        }
+        //마지막 배치 시점 초기화
+        lastSpawnTime = 0f;
+        //다음번 배치까지의 시간 간격을 0으로 초기화
+        timeBetSpawn = 0f;
     }
 
     void Update() {
         // 순서를 돌아가며 주기적으로 발판을 배치
+
+        //게임오버 상태에는 동작하지 않음
+        if (GameManager.instance.isGameover)
+        {
+            return;
+        }
+
+        //마지막 배치 시점에서 timeBetSpawn 이상 시간이 흘렀다면
+        if (Time.time >= lastSpawnTime + timeBetSpawn)
+        {
+            //기록된 마지막 배치 시점을 현재 시점으로 갱신
+            lastSpawnTime = Time.time;
+            //다음 배치 까지의 시간 간격을 timeBetSpawnMin, timeBetSpawnMax);
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+            //배치할 위치의 높이를 
+        }
     }
 }
