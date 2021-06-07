c710273c102cc5befe0995545dc528798965577d
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-06-07 10:33:08 +0900

    Add files via upload

--- PlatformSpawner.cs
+++ PlatformSpawner.cs
@@ -9,8 +9,8 @@ public class PlatformSpawner : MonoBehaviour {
     public float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
     private float timeBetSpawn; // 다음 배치까지의 시간 간격
 
-    public float yMin = -3.5f; // 배치할 위치의 최소 y값
-    public float yMax = 1.5f; // 배치할 위치의 최대 y값
+    public float yMin = -3.0f; // 배치할 위치의 최소 y값
+    public float yMax = 1.0f; // 배치할 위치의 최대 y값
     private float xPos = 20f; // 배치할 위치의 x 값
 
     private GameObject[] platforms; // 미리 생성한 발판들
